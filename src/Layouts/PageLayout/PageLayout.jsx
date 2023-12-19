4beaf355cacd586dac98594f51f5cab3989dd076
anlety <79236211+anlety@users.noreply.github.com>
2023-12-19 22:07:15 +1100

    test2

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Spinner } from "@chakra-ui/react";
+// import Sidebar from "../../components/Sidebar/Sidebar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
