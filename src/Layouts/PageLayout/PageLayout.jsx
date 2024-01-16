770aecf1c6d33081a25a1132894ac4b1c0a92c79
anlety <79236211+anlety@users.noreply.github.com>
2024-01-16 17:24:14 +1100

    test

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,13 +1,11 @@
 import { Box, Flex, Spinner } from "@chakra-ui/react";
-// import Sidebar from "../../components/Sidebar/Sidebar";
-// import Sidebar from "../../components/Sidebar/Sidebar";
-import Sidebar from "../../components/Sidebar/Sidebar"
+import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
 import Navbar from "../../components/Navbar/Navbar";
 
-
+// instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
 
 const PageLayout = ({ children }) => {
 	const { pathname } = useLocation();
